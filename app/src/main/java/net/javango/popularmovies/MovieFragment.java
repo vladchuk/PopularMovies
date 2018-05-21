437721c2f2ff239642da788e70b2afe82b4d9678
vladchuk <vladchuk@javango.net>
2018-05-20 22:50:06 -0700

    Converted movie view to constraint layout, added up navigation.

--- app/src/main/java/net/javango/popularmovies/MovieFragment.java
+++ app/src/main/java/net/javango/popularmovies/MovieFragment.java
@@ -32,21 +32,29 @@ public class MovieFragment extends Fragment {
             savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_movie, container, false);
+        TextView title = view.findViewById(R.id.movie_title);
+        ImageView backdrop = view.findViewById(R.id.backdrop_image);
         ImageView poster = view.findViewById(R.id.poster_image);
         TextView rating = view.findViewById(R.id.movie_rating);
+        TextView votes = view.findViewById(R.id.vote_count);
         TextView date = view.findViewById(R.id.movie_date);
         TextView synopsis = view.findViewById(R.id.movie_synopsis);
 
         Movie movie = (Movie) getActivity().getIntent().getSerializableExtra(EXTRA_MOVIE);
-        getActivity().setTitle(movie.getTitle());
+        title.setText(movie.getTitle());
 
-        String posterUri = NetUtil.getBackdropUri(movie.getBackdropPath());
+        String backdropUri = NetUtil.getBackdropUri(movie.getBackdropPath());
+        Picasso.with(getContext()).
+                load(backdropUri).
+                into(backdrop);
+        String posterUri = NetUtil.getBackdropUri(movie.getPosterPath());
         Picasso.with(getContext()).
                 load(posterUri).
                 into(poster);
 
         rating.setText(movie.getRating());
         date.setText(movie.getReleaseDate());
+        votes.setText(movie.getVoteCount());
         synopsis.setText(movie.getSynopsis());
 
         return view;
